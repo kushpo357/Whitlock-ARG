f2644722e385f3c2089d93ba2908d1e808928a24
Bluet122 <agrawalvaibhav2004@gmail.com>
2026-04-10 12:15:26 +0530

    fix: removed absolute blank screen delay on finale trigger

--- arg-frontend/src/pages/Awaken.js
+++ arg-frontend/src/pages/Awaken.js
@@ -68,15 +68,17 @@ const Awaken = ({ session }) => {
       "Welcome to Whitlock."
     ];
 
-    let currentLine = 0;
+    setFinalLines([lines[0]]);
+    let currentLine = 1;
     const interval = setInterval(() => {
       if (currentLine < lines.length) {
-        setFinalLines(prev => [...prev, lines[currentLine]]);
+        const nextLine = lines[currentLine];
+        setFinalLines(prev => [...prev, nextLine]);
         currentLine++;
       } else {
         clearInterval(interval);
       }
-    }, 2000); // 2 second delay between lines
+    }, 2000); // 2 second delay between subsequent lines
   };
 
   const row0 = ['1','2','3','4','5','6','7','8','9','0'];
